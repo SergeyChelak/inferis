4542387b4dc711a6033aeecb8ec78d448cb43d25
Sergey Chelak <sergey.chelak@gmail.com>
2024-03-31 16:46:15 +0300

    gradient as parameter

--- inferis/src/gameplay/renderer/mod.rs
+++ inferis/src/gameplay/renderer/mod.rs
@@ -45,41 +45,42 @@ pub fn render_scene(
     context.canvas.set_draw_color(Color::BLACK);
     context.canvas.clear();
     render_sky(&mut context)?;
-    render_floor(&mut context)?;
+    render_floor(&mut context, true)?;
     render_game_objects(&mut context)?;
     render_minimap(&mut context)?;
     context.canvas.present();
     Ok(())
 }
 
-fn render_floor(context: &mut RendererContext) -> EngineResult<()> {
+fn render_floor(context: &mut RendererContext, gradient: bool) -> EngineResult<()> {
     let window_size = context.window_size;
     let half_height = window_size.height >> 1;
     let dst = Rect::new(0, half_height as i32, window_size.width, half_height);
-
-    // solid floor
-    // let Some(&color) = context.assets.color("floor") else {
-    //     return Err(EngineError::ResourceNotFound("floor".to_string()));
-    // };
-    // context.canvas.set_draw_color(color);
-    // context
-    //     .canvas
-    //     .fill_rect(dst)
-    //     .map_err(|e| EngineError::Sdl(e.to_string()))?;
-
-    // gradient floor
-    let Some(texture) = context.assets.texture("floor_grad") else {
-        return Err(EngineError::TextureNotFound("floor_grad".to_string()));
-    };
-    let src = {
-        let query = texture.query();
-        let (w, h) = (query.width, query.height);
-        Rect::new(0, 0, w, h)
-    };
-    context
-        .canvas
-        .copy(texture, src, dst)
-        .map_err(|e| EngineError::Sdl(e.to_string()))?;
+    if gradient {
+        // gradient floor
+        let Some(texture) = context.assets.texture("floor_grad") else {
+            return Err(EngineError::TextureNotFound("floor_grad".to_string()));
+        };
+        let src = {
+            let query = texture.query();
+            let (w, h) = (query.width, query.height);
+            Rect::new(0, 0, w, h)
+        };
+        context
+            .canvas
+            .copy(texture, src, dst)
+            .map_err(|e| EngineError::Sdl(e.to_string()))?;
+    } else {
+        // solid floor
+        let Some(&color) = context.assets.color("floor") else {
+            return Err(EngineError::ResourceNotFound("floor".to_string()));
+        };
+        context.canvas.set_draw_color(color);
+        context
+            .canvas
+            .fill_rect(dst)
+            .map_err(|e| EngineError::Sdl(e.to_string()))?;
+    }
     Ok(())
 }
 
