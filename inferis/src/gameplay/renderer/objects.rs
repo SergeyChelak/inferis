8506042e9ad5e07f6dd5bba2c61bafd7651ffce7
Sergey Chelak <sergey.chelak@gmail.com>
2024-04-05 01:54:44 +0300

    depth-based rendering

--- inferis/src/gameplay/renderer/objects.rs
+++ inferis/src/gameplay/renderer/objects.rs
@@ -1,4 +1,4 @@
-use std::f32::consts::PI;
+use std::{cmp::Ordering, f32::consts::PI};
 
 use engine::{rect::Rect, render::Texture, EngineError, EngineResult, Float, Query, Size, Vec2f};
 
@@ -9,20 +9,37 @@ use crate::gameplay::{
 
 use super::{RendererContext, HALF_FIELD_OF_VIEW};
 
+struct TextureRendererTask<'a> {
+    texture: &'a Texture<'a>,
+    source: Rect,
+    destination: Rect,
+    depth: Float,
+}
+
 pub fn render_game_objects(context: &mut RendererContext) -> EngineResult<()> {
-    render_walls(context)?;
-    render_sprites(context)?;
+    let mut tasks = Vec::<TextureRendererTask>::new();
+    render_walls(context, &mut tasks)?;
+    render_sprites(context, &mut tasks)?;
+    tasks.sort_by(|a, b| b.depth.partial_cmp(&a.depth).unwrap_or(Ordering::Equal));
+    for task in tasks {
+        context
+            .canvas
+            .copy(task.texture, task.source, task.destination)
+            .map_err(|e| EngineError::Sdl(e.to_string()))?;
+    }
     Ok(())
 }
 
-fn render_sprites(context: &mut RendererContext) -> EngineResult<()> {
+fn render_sprites<'a>(
+    context: &mut RendererContext<'a>,
+    tasks: &mut Vec<TextureRendererTask<'a>>,
+) -> EngineResult<()> {
     let storage = context.storage;
-
     let player_id = context.player_id;
     let Some(player_pos) = storage.get::<Position>(player_id).and_then(|x| Some(x.0)) else {
         return Err(EngineError::ComponentNotFound("Position".to_string()));
     };
-    // player angle must positive
+    // player angle must be positive
     let Some(player_angle) = storage
         .get::<Angle>(player_id)
         .and_then(|x| Some(x.0))
@@ -65,9 +82,7 @@ fn render_sprites(context: &mut RendererContext) -> EngineResult<()> {
             width: w,
             height: h,
         } = texture_size(texture);
-        // if !(-half_width < x && x < (w + half_width) && norm_dist > 0.5) {
-        //     return Ok(());
-        // }
+        // TODO: skip rendering if sprite is out of screen
         let ratio = w as Float / h as Float;
         // ????
         let sprite_scale = 0.7;
@@ -77,18 +92,21 @@ fn render_sprites(context: &mut RendererContext) -> EngineResult<()> {
         let sprite_half_width = 0.5 * proj_width;
         let sx = x - sprite_half_width;
         let sy = (context.window_size.height as Float - proj_height) * 0.5;
-
-        let dst = Rect::new(sx as i32, sy as i32, proj_width as u32, proj_height as u32);
-        let src = Rect::new(0, 0, w as u32, h);
-        context
-            .canvas
-            .copy(texture, src, dst)
-            .map_err(|e| EngineError::Sdl(e.to_string()))?;
+        let task = TextureRendererTask {
+            texture,
+            source: Rect::new(0, 0, w as u32, h),
+            destination: Rect::new(sx as i32, sy as i32, proj_width as u32, proj_height as u32),
+            depth: norm_distance,
+        };
+        tasks.push(task);
     }
     Ok(())
 }
 
-fn render_walls(context: &mut RendererContext) -> EngineResult<()> {
+fn render_walls<'a>(
+    context: &mut RendererContext<'a>,
+    tasks: &mut Vec<TextureRendererTask<'a>>,
+) -> EngineResult<()> {
     let storage = context.storage;
     let player_id = context.player_id;
     let Some(pos) = storage.get::<Position>(player_id).and_then(|x| Some(x.0)) else {
@@ -135,10 +153,13 @@ fn render_walls(context: &mut RendererContext) -> EngineResult<()> {
             image_width,
             h,
         );
-        context
-            .canvas
-            .copy(texture, src, dst)
-            .map_err(|e| EngineError::Sdl(e.to_string()))?;
+        let task = TextureRendererTask {
+            texture,
+            source: src,
+            destination: dst,
+            depth,
+        };
+        tasks.push(task);
 
         ray_angle += ray_angle_step;
     }
