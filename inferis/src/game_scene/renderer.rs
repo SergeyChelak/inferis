03c676b35ea0391556d58c156d49bb427902820e
Sergey Chelak <sergey.chelak@gmail.com>
2024-05-10 23:41:36 +0300

    porting sky rendering

--- inferis/src/game_scene/renderer.rs
+++ inferis/src/game_scene/renderer.rs
@@ -1,12 +1,15 @@
 use std::{collections::HashMap, f32::consts::PI};
 
 use engine::{
+    rect::Rect,
     systems::{vec_ptr, GameRendererSystem, RendererEffect, VecPtr},
     texture_size, AssetManager, ComponentStorage, EngineError, EngineResult, EntityID, Float,
     SizeU32,
 };
 
-use crate::game_scene::fetch_player_id;
+use crate::{game_scene::fetch_player_id, resource::WORLD_SKY};
+
+use super::components;
 
 const FIELD_OF_VIEW: Float = PI / 3.0;
 const HALF_FIELD_OF_VIEW: Float = FIELD_OF_VIEW * 0.5;
@@ -16,6 +19,9 @@ const MAP_SCALE: u32 = 6;
 pub struct RendererSystem {
     effect_buffer: VecPtr<RendererEffect>,
     texture_size: HashMap<String, SizeU32>,
+    // short cached values
+    angle: Float,
+    // long cached values
     player_id: EntityID,
     window_size: SizeU32,
     rays_count: u32,
@@ -51,6 +57,57 @@ impl RendererSystem {
         }
         Ok(())
     }
+
+    fn render_background(&mut self) -> EngineResult<()> {
+        //let mut buffer = self.effect_buffer.borrow_mut();
+        self.render_floor()?;
+        self.render_sky()?;
+        Ok(())
+    }
+
+    fn render_floor(&mut self) -> EngineResult<()> {
+        //let mut buffer = self.effect_buffer.borrow_mut();
+
+        Ok(())
+    }
+
+    fn render_sky(&mut self) -> EngineResult<()> {
+        let Some(texture_size) = self.texture_size.get(WORLD_SKY) else {
+            return Ok(());
+        };
+        let offset = {
+            let w = self.window_size.width as Float;
+            let offset = -(1.5 * self.angle * w / PI) % w;
+            offset as i32
+        };
+        let SizeU32 {
+            width: w,
+            height: h,
+        } = *texture_size;
+        let source = Rect::new(0, 0, w, h);
+        let half_height = self.window_size.height >> 1;
+        let destinations = [
+            Rect::new(offset, 0, self.window_size.width, half_height),
+            Rect::new(
+                offset - self.window_size.width as i32,
+                0,
+                self.window_size.width,
+                half_height,
+            ),
+            Rect::new(
+                offset + self.window_size.width as i32,
+                0,
+                self.window_size.width,
+                half_height,
+            ),
+        ];
+        for destination in destinations {
+            //
+        }
+        //let mut buffer = self.effect_buffer.borrow_mut();
+
+        Ok(())
+    }
 }
 
 impl GameRendererSystem for RendererSystem {
@@ -77,8 +134,13 @@ impl GameRendererSystem for RendererSystem {
         storage: &ComponentStorage,
         asset_manager: &AssetManager,
     ) -> EngineResult<VecPtr<RendererEffect>> {
-        let mut buffer = self.effect_buffer.borrow_mut();
-        buffer.clear();
+        // prefetch
+        self.angle = storage
+            .get::<components::Angle>(self.player_id)
+            .map(|x| x.0)
+            .ok_or(EngineError::component_not_found("[v2.renderer] angle"))?;
+
+        self.effect_buffer.borrow_mut().clear();
         // TODO: ...
         Ok(self.effect_buffer.clone())
     }
