7aaf8d3b62a2d75c9595f3e41a2ca5757fd66191
Sergey Chelak <sergey.chelak@gmail.com>
2024-05-19 00:01:41 +0300

    clean up debug level

--- inferis/src/resource.rs
+++ inferis/src/resource.rs
@@ -15,9 +15,7 @@ pub const WORLD_SKY: &str = "sky";
 pub const WORLD_FLOOR_GRADIENT: &str = "floor_grad";
 pub const WORLD_TORCH_RED_ANIM: &str = "anim_torch_red";
 pub const WORLD_TORCH_GREEN_ANIM: &str = "anim_torch_green";
-// pub const WORLD_CANDELABRA: &str = "candelabra";
 pub const WORLD_GAME_OVER: &str = "game_over";
-// pub const WORLD_LEVEL_BASIC: &str = "level_basic";
 
 // player
 pub const PLAYER_SHOTGUN_SHOT_ANIM: &str = "anim_shotgun_shot";
